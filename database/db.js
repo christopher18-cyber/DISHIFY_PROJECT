3eda4907cfd51cf02732eb5555ff478f0bd8474e
christopher18-cyber <christopheradegoke4@gmail.com>
2026-04-24 18:10:08 +0100

    fixed a bug

--- database/db.js
+++ database/db.js
@@ -6,7 +6,7 @@ const uri = process.env.MONGODB_URL
 export async function connectToDB() {
     try {
         await mongoose.connect(uri)
-        logger.info(`MongoDB connected successfully.`)
+        logger.info(`MongoDB connected successfully.`, mongoose.connection.name)
     }
     catch (err) {
         logger.error(`MongoDB connection failed.`, err)
